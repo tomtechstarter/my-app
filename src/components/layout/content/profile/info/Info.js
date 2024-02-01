9317df1920056722edf31de3bbac2fdb608d8c7c
SirKaktor <juliuststelldinger@web.de>
2024-02-01 12:53:09 +0100

    info-component added

---
+++ src/components/layout/content/profile/info/Info.js
@@ -0,0 +1,13 @@
+import styles from "./Info.module.css";
+
+function Info() {
+  return (
+    <div>
+      {/* Interactions */}
+      <Statistics></Statistics>
+      {/* Description */}
+    </div>
+  );
+}
+
+export default Info;
