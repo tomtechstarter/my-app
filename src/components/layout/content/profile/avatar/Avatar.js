6a82971151f72176ea1d79069d3c5da86787c801
SirKaktor <juliuststelldinger@web.de>
2024-02-06 14:48:09 +0100

    aufgabe 1.2: image aus datenbank

--- src/components/layout/content/profile/avatar/Avatar.js
+++ src/components/layout/content/profile/avatar/Avatar.js
@@ -1,8 +1,8 @@
 import styles from "./Avatar.module.css";
 
 // Aufgabe 1.2: Image aus Datenbank nutzen
-function Avatar() {
-  return <img className={styles.mainContainer} src="" alt="avatar" />;
+function Avatar({ image }) {
+  return <img className={styles.mainContainer} src={image} alt="avatar" />;
 }
 
 export default Avatar;
